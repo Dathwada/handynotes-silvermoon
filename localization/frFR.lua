1f1d17af0de7c8f6dd9b18a2c41fb9137521ab7b
Machou <Machou@users.noreply.github.com>
2026-03-13 20:50:57 +0100

    Update frFR.lua

--- localization/frFR.lua
+++ localization/frFR.lua
@@ -18,92 +18,92 @@ L["config_icon_scale_desc"] = "Échelle des icônes"
 L["config_icon_alpha"] = "Transparence de l’icône"
 L["config_icon_alpha_desc"] = "Transparence des icônes"
 L["config_what_to_display"] = "Que souhaitez-vous afficher ?"
-L["config_what_to_display_desc"] = "Ces paramètres déterminent le type d’icônes à afficher."
+L["config_what_to_display_desc"] = "Ces paramètres contrôlent quels types d’icônes doivent être affichés."
 
 L["config_auctioneer"] = "Commissaire-priseur"
-L["config_auctioneer_desc"] = "Affiche l’emplacement du commissaire-priseur."
+L["config_auctioneer_desc"] = "Afficher les emplacements des commissaires-priseurs."
 
 L["config_banker"] = "Banque"
-L["config_banker_desc"] = "Affiche l’emplacement de la banque."
+L["config_banker_desc"] = "Afficher les emplacements de la banque."
 
 L["config_barber"] = "Salon de coiffure"
-L["config_barber_desc"] = "Affiche l’emplacement du salon de coiffure."
+L["config_barber_desc"] = "Afficher l’emplacement du salon de coiffure."
 
 L["config_craftingorders"] = "Commandes d’artisanat"
-L["config_craftingorders_desc"] = "Affiche l’emplacement des commandes d’artisanats."
+L["config_craftingorders_desc"] = "Afficher les emplacements des commandes d’artisanat."
 
 L["config_flightmaster"] = "Maître de vol"
-L["config_flightmaster_desc"] = "Affiche l’emplacement du maître de vol."
+L["config_flightmaster_desc"] = "Afficher l’emplacement du maître de vol."
 
-L["config_guildvault"] = "Coffre-fort de guilde"
-L["config_guildvault_desc"] = "Affiche l’emplacement du coffre-fort de guilde."
+L["config_guildvault"] = "Coffre de guilde"
+L["config_guildvault_desc"] = "Afficher l’emplacement du coffre de guilde."
 
 L["config_innkeeper"] = "Aubergiste"
-L["config_innkeeper_desc"] = "Affiche l’emplacement des aubergistes."
+L["config_innkeeper_desc"] = "Afficher les emplacements des aubergistes."
 
 L["config_mail"] = "Boîte aux lettres"
-L["config_mail_desc"] = "Affiche l’emplacement des boîtes aux lettres."
+L["config_mail_desc"] = "Afficher les emplacements des boîtes aux lettres."
 
-L["config_portal"] = "Portails"
-L["config_portal_desc"] = "Affiche l’emplacement des portails."
+L["config_portal"] = "Portail"
+L["config_portal_desc"] = "Afficher les emplacements des portails."
 
-L["config_portaltrainer"] = "Maître des Portails"
-L["config_portaltrainer_desc"] = "Affiche l’emplacement du maître des portails des mages."
+L["config_portaltrainer"] = "Maître des portails"
+L["config_portaltrainer_desc"] = "Afficher l’emplacement du maître des portails des mages."
 
 L["config_tpplatform"] = "Plateforme de téléportation"
-L["config_tpplatform_desc"] = "Affiche l’emplacement de la plateforme de téléportation."
+L["config_tpplatform_desc"] = "Afficher l’emplacement de la plateforme de téléportation."
 
 L["config_travelguide_note"] = "|cFFFF0000*Déjà actif via HandyNotes: TravelGuide.|r"
 
 L["config_reforge"] = "Améliorations d’objets"
-L["config_reforge_desc"] = "Affiche l’emplacement de l’améliorations d’objets."
+L["config_reforge_desc"] = "Afficher l’emplacement de l’améliorations d’objets."
 
 L["config_rostrum"] = "Tribune de transformation"
-L["config_rostrum_desc"] = "Affiche l’emplacement de la tribune de transformation."
+L["config_rostrum_desc"] = "Afficher l’emplacement de la tribune de transformation."
 
 L["config_stablemaster"] = "Maître des écuries"
-L["config_stablemaster_desc"] = "Affiche l’emplacement du maître des écuries."
+L["config_stablemaster_desc"] = "Afficher l’emplacement du maître des écuries."
 
 L["config_trainer"] = "Maître des métiers"
-L["config_trainer_desc"] = "Affiche l’emplacements des maîtres des métiers."
+L["config_trainer_desc"] = "Afficher les emplacements des maîtres des métiers."
 
 L["config_transmogrifier"] = "Transmogrifieur"
-L["config_transmogrifier_desc"] = "Affiche l’emplacement du transmogrifieur."
+L["config_transmogrifier_desc"] = "Afficher l’emplacement du transmogrifieur."
 
 L["config_vendor"] = "Vendeurs"
-L["config_vendor_desc"] = "Affiche l’emplacements des vendeurs."
+L["config_vendor_desc"] = "Afficher les emplacements des vendeurs."
 
 L["config_others"] = "Autres"
-L["config_others_desc"] = "Affiche l’emplacements des endroits importants."
+L["config_others_desc"] = "Afficher tous les autres points d’intérêt."
 
-L["config_onlymytrainers"] = "Affiche uniquement les maîtres de métiers et vendeurs pour mes métiers"
+L["config_onlymytrainers"] = "Afficher uniquement les maîtres et vendeurs pour mes métiers"
 L["config_onlymytrainers_desc"] = [[
-N’affecte que les maîtres des métiers et les vendeurs de mes métiers principales.
+Affecte uniquement les maîtres et vendeurs des métiers principaux.
 
-|cFFFF0000NOTE: n’affecte uniquement lorsque 2 métiers ont été appris.|r
+|cFFFF0000NOTE : Fonctionne seulement lorsque deux métiers principaux sont appris.|r
 ]]
 
-L["config_fmaster_waypoint"] = "Point de passage du Maître de vol"
-L["config_fmaster_waypoint_desc"] = "Définit automatiquement un point de passage vers le maître de vol si vous entrez dans l'Anneau de Transfert."
+L["config_fmaster_waypoint"] = "Point de passage vers le maître de vol"
+L["config_fmaster_waypoint_desc"] = "Définit automatiquement un point de passage vers le maître de vol si vous entrez dans l’Anneau de transfert."
 
-L["config_easy_waypoints"] = "Points de passages faciles"
-L["config_easy_waypoints_desc"] = "Active la création simplifiée de points de passage.\nClic droit pour définir un point de passage et Ctrl + Clic droit pour accéder à plus d’options."
+L["config_easy_waypoints"] = "Points de passage simplifiés"
+L["config_easy_waypoints_desc"] = "Active la création simplifiée de points de passage.\nPermet de définir un point de passage avec Clic droit et d’accéder à plus d’options avec Ctrl + Clic droit."
 
 L["config_waypoint_dropdown"] = "Choisir"
-L["config_waypoint_dropdown_desc"] = "Choisir comment le point de passage va être créé."
+L["config_waypoint_dropdown_desc"] = "Choisissez comment le point de passage doit être créé."
 L["Blizzard"] = true
 L["TomTom"] = true
-L["Both"] = "Les deux"
+L["Both"] = true
 
-L["config_picons"] = "Affiche les icônes de métiers pour :"
-L["config_picons_vendor_desc"] = "Afficher les icônes de métiers pour les vendeurs au lieu des icônes des vendeurs."
-L["config_picons_trainer_desc"] = "Afficher les icônes de métiers pour les maîtres de métiers au lieu des icônes des maîtres de métiers."
-L["config_use_old_picons"] = "Affiche les anciennes icônes de métiers"
-L["config_use_old_picons_desc"] = "Affiche à nouveau les anciennes icônes de métiers au lieu des nouvelles (avant Dragonflight)."
+L["config_picons"] = "Afficher les icônes de métier pour :"
+L["config_picons_vendor_desc"] = "Afficher les icônes de métier pour les vendeurs (au lieu des icônes de vendeur)."
+L["config_picons_trainer_desc"] = "Afficher les icônes de métier pour les maîtres de métier (au lieu des icônes de maître de métier)."
+L["config_use_old_picons"] = "Afficher les anciennes icônes de métier"
+L["config_use_old_picons_desc"] = "Afficher à nouveau les anciennes icônes de métier au lieu des nouvelles (avant Dragonflight)."
 
-L["config_restore_nodes"] = "Restaurer les nœuds cachés"
-L["config_restore_nodes_desc"] = "Restaurer tous les nœuds qui étaient masqués via le menu contextuel."
-L["config_restore_nodes_print"] = "Tous les nœuds cachés ont été restaurés"
+L["config_restore_nodes"] = "Restaurer les points d’intérêt cachés"
+L["config_restore_nodes_desc"] = "Restaurer tous les points d’intérêt qui ont été masqués via le menu contextuel."
+L["config_restore_nodes_print"] = "Tous les points d’intérêt cachés ont été restaurés"
 
 ----------------------------------------------------------------------------------------------------
 -------------------------------------------------DEV------------------------------------------------
@@ -111,8 +111,8 @@ L["config_restore_nodes_print"] = "Tous les nœuds cachés ont été restaurés"
 
 L["dev_config_tab"] = "DEV"
 
-L["dev_config_force_nodes"] = "Forcer les Nœuds"
-L["dev_config_force_nodes_desc"] = "Forcer l’affichage de tous les nœuds, indépendamment de la classe, de la faction ou de la congrégation."
+L["dev_config_force_nodes"] = "Forcer les points d’intérêt"
+L["dev_config_force_nodes_desc"] = "Forcer l’affichage de tous les points d’intérêt, indépendamment de la classe, de la faction ou de la congrégation."
 
 L["dev_config_show_prints"] = "Afficher print()"
 L["dev_config_show_prints_desc"] = "Afficher les messages print() dans la fenêtre de chat."
@@ -123,10 +123,10 @@ L["dev_config_show_prints_desc"] = "Afficher les messages print() dans la fenêt
 
 --==========================================CONTEXT_MENU==========================================--
 
-L["handler_context_menu_addon_name"] = "HandyNotes : Silvermoon"
+L["handler_context_menu_addon_name"] = "HandyNotes : Lune-d’Argent"
 L["handler_context_menu_add_tomtom"] = "Ajouter à TomTom"
 L["handler_context_menu_add_map_pin"] = "Définir un point de passage sur la carte"
-L["handler_context_menu_hide_node"] = "Masquer ce nœud"
+L["handler_context_menu_hide_node"] = "Masquer ce point d’intérêt"
 
 --============================================TOOLTIPS============================================--
 
